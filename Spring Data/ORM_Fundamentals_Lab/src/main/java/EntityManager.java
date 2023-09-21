c209232e1568761405ec985ab38b457457faa99c
idaki <idaki@me.com>
2023-09-21 16:05:18 +0200

    Update EntityManager.java

--- Spring Data/ORM_Fundamentals_Lab/src/main/java/EntityManager.java
+++ Spring Data/ORM_Fundamentals_Lab/src/main/java/EntityManager.java
@@ -27,8 +27,8 @@ public class EntityManager implements DbContext {
             return insertId(entity);
         }
 
-        return ++++++++updateEntity(entity);
-        ;
+        return false; //++++++updateEntity(entity);
+
     }
 
 
