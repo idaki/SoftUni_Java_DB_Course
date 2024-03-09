e2c935c2937f151d073e3bd2d9088fd379e7ea59
Idaki Milev <idaki@me.com>
2024-03-09 22:01:32 +0200

    SpringDataLab

---
+++ Spring Data/SprringDataIntrroLab/src/main/java/bg/softuni/sprringdataintrrolab/SprringDataIntrroLabApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.sprringdataintrrolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprringDataIntrroLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprringDataIntrroLabApplication.class, args);
+	}
+
+}
---
+++ Spring Data/SprringDataIntrroLab/src/test/java/bg/softuni/sprringdataintrrolab/SprringDataIntrroLabApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.sprringdataintrrolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprringDataIntrroLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
